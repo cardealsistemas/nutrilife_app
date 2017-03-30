b0f30ae2537e17ed0a2468dcc0e92d6d97255fc1
Jadson Firmo <jadson.uefs@gmail.com>
2017-03-30 03:29:11 -0300

    Iniciando menu lateral.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { Page3 } from '../pages/page3/page3';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +14,8 @@ import {ServiceProvider} from '../providers/service-provider';
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    Page3
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -22,7 +24,8 @@ import {ServiceProvider} from '../providers/service-provider';
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    Page3
   ],
   providers: [
     StatusBar,
