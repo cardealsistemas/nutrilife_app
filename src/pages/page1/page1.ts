ba55c06f7413d31d0dbc35e2b35b2a593fff7625
Macaule Firmo Oliveira <macaule.firmo@gmail.com>
2017-04-01 21:30:58 -0300

    Slide

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
-
 import {ServiceProvider} from '../../providers/service-provider';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-page1',
@@ -10,6 +10,7 @@ import {ServiceProvider} from '../../providers/service-provider';
 })
 export class Page1 {
 
+  @ViewChild(Slides) slides: Slides;
   categorias : any[];
 
   constructor(public navCtrl: NavController, public service: ServiceProvider) {
